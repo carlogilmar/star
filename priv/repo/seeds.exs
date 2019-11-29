36e112fc0f75b2e3e58fde2c95faacb0f8782dab
Carlo Gilmar <carlogilmar12@gmail.com>
2019-11-29 17:05:58 -0600

    Adding seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,4 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+Star.UserOperator.create_user("makingdevs", "A Star is born", "m4k1nd3vs", "ADMIN")
